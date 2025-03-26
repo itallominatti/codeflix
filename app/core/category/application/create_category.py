9350f0b8cfd0ca5819abe70de9b09f1dc245a4e2
itallominatti <itallominatti@gmail.com>
2025-03-26 08:36:48 -0300

    [ main ] feat: implementing more cases for testing create_category

--- app/core/category/application/create_category.py
+++ app/core/category/application/create_category.py
@@ -2,10 +2,18 @@ from uuid import UUID
 
 from app.core.category.domain.category import Category
 
-def create_category(name: str, description: str, is_active: bool = True) -> UUID:
-    category = Category(
-        name=name,
-        description=description,
-        is_active=is_active
-    )
+
+class InvalidCategoryData(Exception):
+    pass
+
+def create_category(name: str, description: str = "", is_active: bool = True) -> UUID:
+    try:
+        category = Category(
+            name=name,
+            description=description,
+            is_active=is_active
+        )
+    except ValueError as err:
+        raise InvalidCategoryData(err)
+
     return category.id
--- app/core/category/tests/application/test_create_category.py
+++ app/core/category/tests/application/test_create_category.py
@@ -1,6 +1,9 @@
 from uuid import UUID
 
+import pytest
+
 from app.core.category.application.create_category import create_category
+from app.core.category.application.create_category import InvalidCategoryData
 
 class TestCreateCategory:
     def test_create_category_with_valid_data(self):
@@ -11,3 +14,12 @@ class TestCreateCategory:
 
         assert category_id is not None
         assert isinstance(category_id, UUID)
+
+    def test_create_category_with_invalid_data(self):
+        with pytest.raises(InvalidCategoryData, match="name cannot be empty") as exc_info:
+            category_id = create_category(
+                name=""
+            )
+
+            assert exc_info.type is InvalidCategoryData
+            assert str(exc_info.value) == "name cannot be empty"
