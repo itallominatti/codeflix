a3bf3d5b284cfff733cb6b1cbac2254209c78abb
itallominatti <itallominatti@gmail.com>
2025-03-26 08:20:56 -0300

    [ main ] feat: implementing create_category and tests for testing the create_category

---
+++ app/core/category/application/create_category.py
@@ -0,0 +1,11 @@
+from uuid import UUID
+
+from app.core.category.domain.category import Category
+
+def create_category(name: str, description: str, is_active: bool = True) -> UUID:
+    category = Category(
+        name=name,
+        description=description,
+        is_active=is_active
+    )
+    return category.id
